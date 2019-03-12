6e63e0c3919d76969ef990ab3d87c0f85cc2a218
Ioannis-Rafail Mitromaras <mitromarasg96@gmail.com>
2019-03-12 15:59:26 +0200

    Delete build_splitNSPgui.py

--- build_splitNSPgui.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-build_prc = subprocess.Popen(
-    'pyinstaller -y -F -n splitNSP --distpath . --specpath ./build '
-    '"C:\\Users\\johnm\\PycharmProjects\\splitNSP\\splitNSPgui.py"')
-return_code = build_prc.wait()
-
-print(f"Build ended with {return_code}")
